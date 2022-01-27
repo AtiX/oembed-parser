9d06ccacf6e4537ed2eef314742c669df221d6d8
Yannis Kommana <yannis.kommana@sprylab.com>
2022-01-27 16:23:55 +0100

    Reformat webpack.config.js to comply with linter settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    entry: './index.js',
-    target: 'node',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        library: {
-            name: 'oembed-parser',
-            type: 'umd'
-        },
-    }
-}
+  entry: './index.js',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    library: {
+      name: 'oembed-parser',
+      type: 'umd',
+    },
+  },
+};
